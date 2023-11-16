e92316920dd8c415fb0191ab1899b9710cabfdc8
yumeminami <shikinamiasuka1103@gmail.com>
2023-11-16 18:29:08 +0800

    feature/Add nginx configuration and Docker Compose file for patrol-robot-admin

--- config-overrides.js
+++ config-overrides.js
@@ -13,7 +13,7 @@ const addCustomize = () => (config) => {
   if (config.output.publicPath) {
     config.output.publicPath =
       process.env.NODE_ENV === "production"
-        ? "/react-antd-admin-template/"
+        ? "/patrol-robot-admin/"
         : "/";
   }
   if (config.resolve) {
