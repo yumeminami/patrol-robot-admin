cde0b337bddf9cef2c6d42e7cf5a4042cf0d5b6a
yumeminami <shikinamiasuka1103@gmail.com>
2023-12-06 14:49:36 +0800

    update/Update docker-compose.yml and login page

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -110,13 +110,13 @@ const Login = (props) => {
                 登录
               </Button>
             </Form.Item>
-            <Form.Item>
+            {/* <Form.Item>
               <span>账号 : admin 密码 : 随便填</span>
               <br />
               <span>账号 : editor 密码 : 随便填</span>
               <br />
               <span>账号 : guest 密码 : 随便填</span>
-            </Form.Item>
+            </Form.Item> */}
           </Spin>
         </Form>
       </div>
